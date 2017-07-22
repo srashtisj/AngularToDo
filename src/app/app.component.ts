4d91c559864c70669e1515c8528abd80785375ca
Srashti Jain <srashti.jain@newcon.com>
2017-07-22 16:49:51 +0530

    Added dashboardComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DashboardComponent} from './dashboard/dashboard.component'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Component Git';
+
+  constructor(){}
 }
